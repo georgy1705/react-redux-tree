784f59050fe1406410d81c38cf89d3ab087760c0
georgy1705 <shimgk1705@gmail.com>
2023-01-26 16:36:45 +0300

    feat: Upload file

--- src/hoc/Layout/Layout.tsx
+++ src/hoc/Layout/Layout.tsx
@@ -3,8 +3,8 @@ import { useEffect } from "react"
 import { Tree } from "../../components/Tree/Tree"
 import { TreeCard } from "../../components/TreeCard/TreeCard"
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks"
-import { Properties } from "../../models/models"
-import { fetchData } from "../../store/actions/tree"
+import { Properties, TreeProps } from "../../models/models"
+import { fetchData, fetchDataSuccess } from "../../store/actions/tree"
 import "./Layout.scss"
 
 const Layout = () => {
@@ -17,6 +17,30 @@ const Layout = () => {
     useEffect(() => {
         fetchData(dispatch)
     }, [])
+
+    const downloadFile = () => {
+        let a = document.createElement("a")
+        let file = new Blob([JSON.stringify(treeProperties)], {
+            type: "application/json",
+        })
+        a.href = URL.createObjectURL(file)
+        a.download = "file.json"
+        a.click()
+    }
+
+    const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.files || e.target.files.length === 0) {
+            return
+        }
+
+        const fileReader = new FileReader()
+        fileReader.readAsText(e.target.files[0]!, "UTF-8")
+        fileReader.onload = (e: any) => {
+            const data: TreeProps = JSON.parse(e.target.result)
+
+            dispatch(fetchDataSuccess(data))
+        }
+    }
 
     return (
         <Container
@@ -39,10 +63,22 @@ const Layout = () => {
                     </Grid>
                     <Grid item xs={9}>
                         <div className="header-card">
+                            <Button variant="contained" component="label">
+                                Загрузить файл
+                                <input
+                                    hidden
+                                    accept=".json"
+                                    type="file"
+                                    onChange={handleUpload}
+                                />
+                            </Button>
+
                             <Button
-                                onClick={() => {}}
+                                disabled={treeProperties.length === 0}
+                                onClick={downloadFile}
                                 variant="contained"
                                 color="success"
+                                style={{ marginLeft: 10 }}
                             >
                                 Скачать файл
                             </Button>
